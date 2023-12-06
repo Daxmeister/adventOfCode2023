9d7f49171201bdbc9f4c28a5488c246ac90ab175
Davide Mario Attebrant Sbrzesny <dmas@kth.se>
2023-12-06 09:24:22 +0100

    started day 2

---
+++ Day 2/day2part1.py
@@ -0,0 +1,7 @@
+# Turn every row into an objekt
+# Have attribute "possible"=ture/false
+# Create add method for objects where they only add their id if possible
+
+class Game()
+    def __init__(self, row):
+        pass
